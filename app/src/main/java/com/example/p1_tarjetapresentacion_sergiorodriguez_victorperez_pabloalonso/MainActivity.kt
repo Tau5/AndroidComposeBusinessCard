447e3f5e6bd0a1ef4d970f3ae0c8a94d620fc773
Chuwi12 <srpc9682@protonmail.com>
2025-10-28 13:27:45 +0100

    Second commit

--- app/src/main/java/com/example/p1_tarjetapresentacion_sergiorodriguez_victorperez_pabloalonso/MainActivity.kt
+++ app/src/main/java/com/example/p1_tarjetapresentacion_sergiorodriguez_victorperez_pabloalonso/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.p1_tarjetapresentacion_sergiorodriguez_victorperez_pabloalonso.ui.theme.P1_TarjetaPresentacion_SergioRodriguez_VictorPerez_PabloAlonsoTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,22 +24,43 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             P1_TarjetaPresentacion_SergioRodriguez_VictorPerez_PabloAlonsoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
+fun Greeting(name: String, modifier: Modifier = Modifier, title: String) {
+    val image = painterResource(R.drawable.doge_meme_png_photos_1504254126)
+    Box {
+        Row {
+            Image(
+                painter = image,
+                contentDescription = null,
+                modifier = modifier.s
+            )
+        }
+        Row {
+            Text(
+                text = "$name",
+                modifier = modifier
+                    .padding(25.dp)
+            )
+        }
+        Row {
+           Text(
+               text = "$title",
+               modifier = modifier
+                   .padding(25.dp)
+           )
+        }
+    }
+}
+
+@Composable
+fun Info(telfNumb: String, mail: String, secondMail:String) {
     Text(
-        text = "Hello $name!",
-        modifier = modifier
+        text = telfNumb
     )
 }
 
@@ -42,6 +68,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     P1_TarjetaPresentacion_SergioRodriguez_VictorPerez_PabloAlonsoTheme {
-        Greeting("Android")
+        Greeting(name = stringResource(R.string.pablo_alonso_sergio_rodr_guez_y_victor_p_rez),
+            title = stringResource(
+            R.string.desarrolladores_moviles
+        ))
     }
 }
